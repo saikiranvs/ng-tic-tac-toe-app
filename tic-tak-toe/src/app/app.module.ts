14a2f3e22f6572795641c8ee302346e28b520663
Saikiran Veerabathini <saikiranveerabathini333@gmail.com>
2021-11-20 14:47:11 +0530

    Add files via upload

---
+++ tic-tak-toe/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+//font-awesome
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+//Toaster
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IconComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
